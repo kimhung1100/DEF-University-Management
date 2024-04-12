c37532aef3fe6103a1f8ee6063a8d89739855373
JoyceT1 <116693567+JoyceT1@users.noreply.github.com>
2024-04-13 06:46:47 +0700

    Subject.java tuan dang lam

--- src/main/java/org/defuni/course/Subject.java
+++ src/main/java/org/defuni/course/Subject.java
@@ -1,8 +1,21 @@
 package org.defuni.course;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Subject {
+    private List<StudentObserver> observers;
+
+    public Course() {
+        observers = new ArrayList<>();
+    }
     public void registerObserver(StudentObserver o);
+        observers.add(o);
     public void removeObserver(StudentObserver o);
+        observers.remove(o);
     public void notifyObserver();
+        for (StudentObserver observer : observers) {
+            observer.update();
+        }
 }
 
