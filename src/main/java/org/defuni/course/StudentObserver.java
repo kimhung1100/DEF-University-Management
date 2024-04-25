87674858dcbca702dc874b9e05d929c171806820
Anh Tuan <116693567+JoyceT1@users.noreply.github.com>
2024-04-25 08:07:16 +0700

    Update StudentObserver.java

--- src/main/java/org/defuni/course/StudentObserver.java
+++ src/main/java/org/defuni/course/StudentObserver.java
@@ -1,22 +1,6 @@
 package org.defuni.course;
 
-public class Student implements StudentObserver {
-    private String studentID;
-    private String name;
-
-    public Student(String studentID, String name) {
-        this.studentID = studentID;
-        this.name = name;
-    }
-
-    public void displayGrade() {
-        System.out.println("Grades for Student " + name + ": ");s
-        double[] grades = scheduledClass.getScoreStudent(studentID);
-        for (int i = 0; i < grades.length; i++) {
-            System.out.println("Component " + (i+1) + ": " + grades[i]);
-        }
-    }
-    public void update(ScheduledClass scheduledClass) {
-        displayGrade(); 
-    }
+public interface StudentObserver{
+    public void displayGrade();
+    public void update(ScheduledClass scheduledClass);
 }
